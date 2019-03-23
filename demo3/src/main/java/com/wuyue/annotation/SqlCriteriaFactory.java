e418c1827bf906fc5a43a51d399fb5a420e0823e
Error <2583588839@qq.com>
2019-03-23 21:17:43 +0800

    ws

--- demo3/src/main/java/com/wuyue/annotation/SqlCriteriaFactory.java
+++ demo3/src/main/java/com/wuyue/annotation/SqlCriteriaFactory.java
@@ -21,31 +21,31 @@ public class SqlCriteriaFactory<T extends BasePojo> {
             String key = field.getName();
             String value = (String) ClassUtil.getFieldValByName(t, key);
             SqlWhere sqlWhere = field.getAnnotation(SqlWhere.class);
-            if(sqlWhere!=null){
+            if(value!=null&&sqlWhere!=null){
                 String sqlWhereValue = sqlWhere.value().getValue();
-                if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.LIKE.getValue())){
-                    value = "'%"+value+"%'";
-                }else if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.IN.getValue())){
-                    key = sqlWhere.proprtityName();
-                    value = value.replace("[","(");
-                    value = value.replace("]",")");
-                }else if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.LTDATE.getValue())){
-                    key = "DATE_FORMAT(" + sqlWhere.proprtityName() + ", '%Y-%m-%d')";
-                    value = "DATE_FORMAT('"+value+"','%Y-%m-%d')";
-                }else if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.GTDATE.getValue())){
-                    key = "DATE_FORMAT(" + sqlWhere.proprtityName() + ", '%Y-%m-%d')";
-                    value = "DATE_FORMAT('"+value+"','%Y-%m-%d')";
-                } else{
-                    value = "'"+value+"'";
-                }
+                    if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.LIKE.getValue())){
+                        value = "'%"+value+"%'";
+                    }else if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.IN.getValue())){
+                        key = sqlWhere.proprtityName();
+                        value = value.replace("[","(");
+                        value = value.replace("]",")");
+                    }else if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.LTDATE.getValue())){
+                        key = "DATE_FORMAT(" + sqlWhere.proprtityName() + ", '%Y-%m-%d')";
+                        value = "DATE_FORMAT('"+value+"','%Y-%m-%d')";
+                    }else if(sqlWhereValue.equals(SqlWhere.SqlWhereValue.GTDATE.getValue())){
+                        key = "DATE_FORMAT(" + sqlWhere.proprtityName() + ", '%Y-%m-%d')";
+                        value = "DATE_FORMAT('"+value+"','%Y-%m-%d')";
+                    } else{
+                        value = "'"+value+"'";
+                    }
+                    if(key.contains(sqlWhere.value().name())){
+                        key = key.replace(sqlWhere.value().name(),"");
+                    }
+
+                    criteria.andCondition(key + sqlWhereValue + value);
+
+                    continue;
                 //针对createGT等范围查询需要得到对应的字段，如create
-                if(key.contains(sqlWhere.value().name())){
-                    key = key.replace(sqlWhere.value().name(),"");
-                }
-
-                criteria.andCondition(key + sqlWhereValue + value);
-
-                continue;
             }
 
             criteria.andEqualTo(key, value);
